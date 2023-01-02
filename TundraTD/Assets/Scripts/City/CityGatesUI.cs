d6f473c6f1c6597b3decb004173942013fec3a16
Mikov Denis <denis.mikov.06@gmail.com>
2023-01-02 20:04:31 +0300

    feat(mobs): added city gates hp visualization

---
+++ TundraTD/Assets/Scripts/City/CityGatesUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace City
+{
+    /// <summary>
+    /// Represents the UI behaviour of CityGates
+    /// </summary>
+    [RequireComponent(typeof(CityGates))]
+    public class CityGatesUI : MonoBehaviour
+    {
+        [FormerlySerializedAs("healthPointHolder")] [SerializeField] private Text healthPointsHolder;
+        private CityGates _cityGates;
+
+        public void UpdateHealthText(string text)
+        {
+            healthPointsHolder.text = text;
+        }
+        
+        private void Start()
+        {
+            _cityGates = GetComponent<CityGates>();
+            healthPointsHolder.text = _cityGates.CityGatesHealthPoints.ToString();
+        }
+        
+    }
+}
