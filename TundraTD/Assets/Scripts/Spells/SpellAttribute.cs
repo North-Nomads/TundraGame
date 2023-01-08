b191918d57e577edb1e68c706edf3e023b7f1dd1
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2023-01-08 22:06:36 +0500

    refactor(spells): Cleaned up the code

--- TundraTD/Assets/Scripts/Spells/SpellAttribute.cs
+++ TundraTD/Assets/Scripts/Spells/SpellAttribute.cs
@@ -12,14 +12,17 @@ namespace Spells
         /// A core element used to create a spell.
         /// </summary>
         public BasicElement CoreElement { get; set; }
+
         /// <summary>
         /// A description of a spell.
         /// </summary>
         public string Description { get; set; }
+
         /// <summary>
         /// The name of the spell.
         /// </summary>
         public string Name { get; set; }
+
         /// <summary>
         /// Creates the new instance of a <see cref="SpellAttribute"/> using all provided data.
         /// </summary>
