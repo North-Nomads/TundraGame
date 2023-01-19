f6b1439a96a8f03be37525d3abdda25d89ebcdf3
DaSh-More <danila-763@yandex.ru>
2023-01-19 17:23:37 +0300

    refactor(camera): clear camera code

--- TundraTD/Assets/Scripts/Camera/CameraMotion.cs
+++ TundraTD/Assets/Scripts/Camera/CameraMotion.cs
@@ -4,197 +4,214 @@ using UnityEngine;
 // У нас по-настоящему инклюзивная, гендерно нейтральная команда!
 public class CameraMotion : MonoBehaviour
 {
-    private Camera Cam;
-    private bool Flag;
-    private float XPos;
-    private float ZPos;
-    private Vector2 ChangePos1;
-    private Vector2 ChangePos2;
-    private Touch Touch;
-    private Touch Touch1;
-    private Touch Touch2;
-    private float LastPositionDistance;
-    private float Zoom;
+    private Vector2 _previousCordsOfFirstFinger;
+    private Vector2 _previousCordsOfSecondFinger;
+    private Vector3 _pointToZoom;
+    private Touch _cordsOfFirstFinger;
+    private Touch _cordsOfSecondFinger;
+    private Camera _mainCamera;
+    private float _previousDistanceBetweenTouch;
+    private float _zoom;
+    private float _xDistance;
+    private float _zDistance;
+    private float _xDistanceToMove;
+    private float _zDistanceToMove;
+    private const float _denominatorOfMovementSpeedOnScreen = 300;
+    private const float _cameraMaxSize = 16;
+    private const float _cameraMinSize = 3;
+    private const float _pixelDifferenceToMove = 15;
+    private const float _denominatorOfZoomSpeedOnScreen = 25;
+    private const float _xDenominatorOfZoomSpeedToPoint = 7;
+    private const float _zDenominatorOfZoomSpeedToPoint = 10;
+    private const float _shortageOfZoomToInertia = 1.2f;
+    private const float _shortageOfMovementToInertia = 2;
+    private const float _denominatorOfZoomSpeedOnScreenOnInertia = 200;
+    private const float _xDenominatorOfZoomSpeedToPointOnInertia = 80;
+    private const float _zDenominatorOfZoomSpeedToPointOnInertia = 90;
+
     private void Start()
     {
-        Cam = GetComponent<Camera>();
+        _mainCamera = GetComponent<Camera>();
     }
 
-    private void Update()
+    private void MoveCamera(Touch _touchToMove)
+    {
+        _xDistance =
+            _touchToMove.deltaPosition.x * Mathf.Sin(transform.rotation.y)
+            - _touchToMove.deltaPosition.y;
+        _zDistance =
+            _touchToMove.deltaPosition.x
+            + _touchToMove.deltaPosition.y * Mathf.Sin(transform.rotation.y);
+        transform.position +=
+            new Vector3(
+                _xDistance * _mainCamera.orthographicSize,
+                0,
+                _zDistance * _mainCamera.orthographicSize
+            ) / _denominatorOfMovementSpeedOnScreen;
+    }
+
+    private void ChangeZoom(float _cameraZoomSize)
+    {
+        if (_cameraZoomSize <= _cameraMinSize)
+        {
+            _mainCamera.orthographicSize = _cameraMinSize;
+        }
+        else if (_cameraZoomSize >= _cameraMaxSize)
+        {
+            _mainCamera.orthographicSize = _cameraMaxSize;
+        }
+        else
+        {
+            _mainCamera.orthographicSize = _cameraZoomSize;
+        }
+    }
+
+    private Vector3 GetPointToZoom()
+    {
+        return new Vector3(
+            (
+                _mainCamera.ScreenToWorldPoint(_cordsOfFirstFinger.position).x
+                + _mainCamera.ScreenToWorldPoint(_cordsOfSecondFinger.position).x
+            ) / 2,
+            transform.position.y,
+            (
+                _mainCamera.ScreenToWorldPoint(_cordsOfFirstFinger.position).z
+                + _mainCamera.ScreenToWorldPoint(_cordsOfSecondFinger.position).z
+            ) / 2
+        );
+    }
+
+    private void FixedUpdate()
     {
         if (Input.touchCount == 2)
-        { 
-            // Получаем координаты касаний
-            Touch1 = Input.GetTouch(0);
-            Touch2 = Input.GetTouch(1);
+        {
+            _cordsOfFirstFinger = Input.GetTouch(0);
+            _cordsOfSecondFinger = Input.GetTouch(1);
 
-            // Получаем координаты начала касаний
-            Vector2 TouchDirection1 = Touch1.position - Touch1.deltaPosition;
-            Vector2 TouchDirection2 = Touch2.position - Touch2.deltaPosition;
-
-            // Вычисляем расстояния между текущими касаниями
-            float PositionDistance = Vector2.Distance(Touch1.position, Touch2.position);
-
-            // Вычисляем рассояние между начальными касаниями
-            float DirectionDistance = Vector2.Distance(
-                TouchDirection1,
-                TouchDirection2
-            );
-            
-            // Если позиция изменилась
-            if (ChangePos1 != Touch1.position && ChangePos2 != Touch2.position)
+            if (_cordsOfSecondFinger.position != _previousCordsOfSecondFinger)
             {
-                
-                // Если разница между расстояниями существенна, значит делается зум
-                if (Mathf.Abs(LastPositionDistance - PositionDistance) > 5)
-                {
-                    Zoom = PositionDistance - DirectionDistance;
-                        
-                    // Получаем новый зум камеры
-                    float CamSize = Cam.orthographicSize - Zoom / 75;
-                    // Если зум не слишком маленький ставим его, иначе 3
-                    if (CamSize <= 3)
-                    {
-                        Cam.orthographicSize = 3;
-                    }
-                    else if (CamSize >= 16)
-                    {
-                        Cam.orthographicSize = 16;
-                    }
-                    else
-                    {
-                        Cam.orthographicSize = CamSize;
-                    }
-                    ChangePos1 = Touch1.position;
-                    ChangePos2 = Touch2.position;
-                    LastPositionDistance = PositionDistance;
-                }
+                MoveCamera(_cordsOfSecondFinger);
             }
-            else if ((ChangePos1 == Touch1.position && ChangePos2 != Touch2.position) || (ChangePos1 != Touch1.position && ChangePos2 == Touch2.position))
+
+            float _distanceBetweenTouch = Vector2.Distance(
+                _cordsOfFirstFinger.position,
+                _cordsOfSecondFinger.position
+            );
+
+            float _distanceBetweenMovedFingers = Vector2.Distance(
+                _cordsOfFirstFinger.position - _cordsOfFirstFinger.deltaPosition,
+                _cordsOfSecondFinger.position - _cordsOfSecondFinger.deltaPosition
+            );
+            if (
+                _previousCordsOfFirstFinger != _cordsOfFirstFinger.position
+                || _previousCordsOfSecondFinger != _cordsOfSecondFinger.position
+            )
             {
-
-                // Если разница между расстояниями существенна, значит делается зум
-                if (Mathf.Abs(DirectionDistance - PositionDistance) > 5)
+                if (
+                    Mathf.Abs(_previousDistanceBetweenTouch - _distanceBetweenTouch)
+                    > _pixelDifferenceToMove
+                )
                 {
-                    Zoom = PositionDistance - DirectionDistance;
-
-                    // Получаем новый зум камеры
-                    float CamSize = Cam.orthographicSize - Zoom / 75;
-                    // Если зум не слишком маленький ставим его, иначе 3
-                    if (CamSize <= 3)
+                    _zoom = _distanceBetweenTouch - _distanceBetweenMovedFingers;
+                    float _cameraZoomSize = _mainCamera.orthographicSize;
+                    if (_zoom > 0)
                     {
-                        Cam.orthographicSize = 3;
-                    }
-                    else if (CamSize >= 16)
-                    {
-                        Cam.orthographicSize = 16;
+                        _pointToZoom = GetPointToZoom();
+                        _cameraZoomSize =
+                            _mainCamera.orthographicSize - _zoom / _denominatorOfZoomSpeedOnScreen;
+                        transform.position = new Vector3(
+                            transform.position.x
+                                - (transform.position.x - _pointToZoom.x)
+                                    / _xDenominatorOfZoomSpeedToPoint,
+                            transform.position.y,
+                            transform.position.z
+                                - (transform.position.z - _pointToZoom.z)
+                                    / _zDenominatorOfZoomSpeedToPoint
+                        );
                     }
                     else
                     {
-                        Cam.orthographicSize = CamSize;
+                        _cameraZoomSize =
+                            _mainCamera.orthographicSize - _zoom / _denominatorOfZoomSpeedOnScreen;
                     }
-                    // 7
-                    ChangePos1 = Touch1.position;
-                    ChangePos2 = Touch2.position;
-                    LastPositionDistance = PositionDistance;
+                    ChangeZoom(_cameraZoomSize);
+                    _previousCordsOfFirstFinger = _cordsOfFirstFinger.position;
+                    _previousCordsOfSecondFinger = _cordsOfSecondFinger.position;
+                    _previousDistanceBetweenTouch = _distanceBetweenTouch;
                 }
             }
             else
             {
-                Zoom = 0;
+                _zoom = 0;
             }
         }
-        // Если идет хотя бы одно касание
-        if (Input.touchCount >= 1)
+        // Если есть одно касание
+        if (Input.touchCount == 1)
         {
-            if (Input.touchCount == 1 || Input.touchCount > 2)
+            _cordsOfFirstFinger = Input.GetTouch(0);
+
+            if (_cordsOfFirstFinger.position != _previousCordsOfFirstFinger)
             {
-            Touch1 = Input.GetTouch(0);
-
-                // Если изменилась позиция первого пальца
-                if (Touch1.position != ChangePos1)
-                {
-                    // Двигаем камеру
-                    // HACK
-                    XPos = Touch1.deltaPosition.x * Mathf.Sin(120) - Touch1.deltaPosition.y;
-                    ZPos = Touch1.deltaPosition.x + Touch1.deltaPosition.y * Mathf.Sin(120);
-                    
-                    transform.position = new Vector3(
-                        transform.position.x + XPos * Cam.orthographicSize / 300,
-                        transform.position.y,
-                        transform.position.z + ZPos * Cam.orthographicSize / 300
-                    );
-
-                    // Сохраняем текущую позицию
-                    ChangePos1 = Touch1.position;
-                }
+                MoveCamera(_cordsOfFirstFinger);
+                _previousCordsOfFirstFinger = _cordsOfFirstFinger.position;
             }
-            else if (Input.touchCount == 2)
-            {
-                Touch1 = Input.GetTouch(0);
-                Touch2 = Input.GetTouch(1);
-                if (Touch1.position != ChangePos1)
-                {
-                    Touch = Touch1;
-                    // Сохраняем текущую позицию
-                    ChangePos1 = Touch1.position;
-                    Flag = true;
-                }
-                else if (Touch2.position != ChangePos2)
-                {
-                    Touch = Touch2;
-                    // Сохраняем текущую позицию
-                    ChangePos2 = Touch2.position;
-                    Flag = true;
-                }
-            if (Flag) {
-                XPos = Touch.deltaPosition.x * Mathf.Sin(120) - Touch.deltaPosition.y;
-                ZPos = Touch.deltaPosition.x + Touch.deltaPosition.y * Mathf.Sin(120);
-                transform.position = new Vector3(
-                    transform.position.x + XPos * Cam.orthographicSize / 300,
-                    transform.position.y,
-                    transform.position.z + ZPos * Cam.orthographicSize / 300
-                    );
-                Flag = false;
-            }
-            }
-
         }
         else
         {
-            // Уменьшаешь значения чтобы не слишком быстро менялись
-            if (Math.Abs(Zoom) > 1 || Math.Abs(XPos) > 1 || Math.Abs(ZPos) > 1) {
-                Zoom = Zoom / 1.2f;
-                XPos = XPos / 2f;
-                ZPos = ZPos / 2f;
-            }
-            if (Math.Abs(Zoom) > 1)
+            if (
+                Math.Abs(_zoom) > 1
+                || Math.Abs(_xDistanceToMove) > 1
+                || Math.Abs(_zDistanceToMove) > 1
+            )
             {
-                float CamSize = Cam.orthographicSize - Zoom / 200;
-                if (CamSize <= 3)
-                    {
-                        Cam.orthographicSize = 3;
-                    }
-                    else if (CamSize >= 16)
-                    {
-                        Cam.orthographicSize = 16;
-                    }
-                    else
-                    {
-                        Cam.orthographicSize = CamSize;
-                    }
-                Zoom -= Math.Sign(Zoom);
+                _zoom = _zoom / _shortageOfZoomToInertia;
+                _xDistanceToMove = _xDistanceToMove / _shortageOfMovementToInertia;
+                _zDistanceToMove = _zDistanceToMove / _shortageOfMovementToInertia;
             }
-            if (Math.Abs(XPos) > 1 || Math.Abs(ZPos) > 1)
+            if (Math.Abs(_zoom) > 1)
             {
-                if (Math.Abs(XPos) > 1)
+                float _cameraZoomSize = _mainCamera.orthographicSize;
+
+                if (_zoom > 0)
                 {
-                    transform.position = new Vector3(transform.position.x + XPos / 300 * Cam.orthographicSize, transform.position.y, transform.position.z);
+                    _pointToZoom = GetPointToZoom();
+
+                    _cameraZoomSize =
+                        _mainCamera.orthographicSize
+                        - _zoom / _denominatorOfZoomSpeedOnScreenOnInertia;
+
+                    transform.position = new Vector3(
+                        transform.position.x
+                            - (transform.position.x - _pointToZoom.x)
+                                / _xDenominatorOfZoomSpeedToPointOnInertia,
+                        transform.position.y,
+                        transform.position.z
+                            - (transform.position.z - _pointToZoom.z)
+                                / _zDenominatorOfZoomSpeedToPointOnInertia
+                    );
                 }
-                if (Math.Abs(ZPos) > 1)
+                else
                 {
-                    transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + ZPos / 300 * Cam.orthographicSize);
+                    _cameraZoomSize =
+                        _mainCamera.orthographicSize
+                        - _zoom / _denominatorOfZoomSpeedOnScreenOnInertia;
                 }
+                ChangeZoom(_cameraZoomSize);
+                _zoom -= Math.Sign(_zoom);
+            }
+            if (Math.Abs(_xDistanceToMove) > 1 || Math.Abs(_zDistanceToMove) > 1)
+            {
+                transform.position = new Vector3(
+                    transform.position.x
+                        + _xDistanceToMove
+                            / _denominatorOfMovementSpeedOnScreen
+                            * _mainCamera.orthographicSize,
+                    transform.position.y,
+                    transform.position.z
+                        + _zDistanceToMove
+                            / _denominatorOfMovementSpeedOnScreen
+                            * _mainCamera.orthographicSize
+                );
             }
         }
     }
