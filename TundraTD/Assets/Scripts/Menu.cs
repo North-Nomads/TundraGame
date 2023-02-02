bcb8ad281c3e9e02d29db670660dc681d78a3cc7
Nikitos909 <nikita034@bk.ru>
2023-02-02 17:05:04 +0300

    Main_menu_1

---
+++ TundraTD/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
