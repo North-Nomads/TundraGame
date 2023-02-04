8888d32b928647cfdea30ffc6af281c9094243b6
Mikov Denis <denis.mikov.06@gmail.com>
2023-02-04 09:26:48 +0300

    fix(wave): fixed gates hp bar bug

--- TundraTD/Assets/Scripts/City/CityGatesUI.cs
+++ TundraTD/Assets/Scripts/City/CityGatesUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace City
@@ -11,7 +12,13 @@ namespace City
     {
         [SerializeField] private Image topHealthBar;
         [SerializeField] private Text influencePointsHolder;
-
+        private void Start()
+        {
+            if (topHealthBar.sprite is null)
+                throw new Exception("No sprite was assigned");
+            topHealthBar.fillAmount = 1f;
+        }
+        
         public void UpdateInfluencePointsText(string text)
         {
             influencePointsHolder.text = text;
@@ -22,10 +29,6 @@ namespace City
             topHealthBar.fillAmount = percent;
         }
         
-        private void Start()
-        {
-            topHealthBar.fillAmount = 1f;
-        }
         
     }
 }
