1142c2ebfb0a34a98502d7d7f644042c94b521dc
Nikitos909 <nikita034@bk.ru>
2023-02-04 08:34:48 +0300

    refactoring_add_upgrade_earth_tower

---
+++ TundraTD/Assets/Scripts/City/Building/Upgrades/EarthTowerUpgrades/EarthSlowingEnemiesUpgrade.cs
@@ -0,0 +1,20 @@
+﻿using City.Building.ElementPools;
+using Spells;
+using UnityEngine;
+
+namespace City.Building.Upgrades.EarthTowerUpgrades
+{
+    public class EarthSlowingEnemiesUpgrade : IUpgrade
+    {
+        public BasicElement Element => BasicElement.Earth;
+        public int Price => 200;
+        public int RequiredLevel => 2;
+        public string UpgradeDescriptionText => "Slows down enemies by 10%";
+        public Sprite Sprite => Resources.Load<Sprite>("UpgradeIcons/Arcanist1");
+
+        public void ExecuteOnUpgradeBought()
+        {
+            EarthPool.SlowDownEnemiesTime *= 1.1f;
+        }
+    }
+}
---
+++ TundraTD/Assets/Scripts/City/Building/Upgrades/EarthTowerUpgrades/TimeStunEnemiesUpgrade.cs
@@ -0,0 +1,21 @@
+﻿using City.Building.ElementPools;
+using Spells;
+using UnityEngine;
+
+namespace City.Building.Upgrades.EarthTowerUpgrades
+{
+    public class TimeStunEnemiesUpgrade : IUpgrade
+    {
+        public BasicElement Element => BasicElement.Earth;
+        public int Price => 350;
+        public int RequiredLevel => 3;
+        public string UpgradeDescriptionText => "Increase time stun of enemies by 0.5 second";
+        public Sprite Sprite => Resources.Load<Sprite>("UpgradeIcons/Arcanist1");
+
+        public void ExecuteOnUpgradeBought()
+        {
+            EarthPool.TimeStunEnemies += 0.5f;
+        }
+    }
+}
+
