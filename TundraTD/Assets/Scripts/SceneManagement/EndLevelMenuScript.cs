22123c130f23cfd4f6c72a2963d61afacb9667dc
Mikov Denis <denis.mikov.06@gmail.com>
2023-02-06 23:02:35 +0300

    refactor(static): removed extra files, moved imports

--- TundraTD/Assets/Scripts/SceneManagement/EndLevelMenuScript.cs
+++ TundraTD/Assets/Scripts/SceneManagement/EndLevelMenuScript.cs
@@ -1,21 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndLevelMenuScript : MonoBehaviour
+namespace SceneManagement
 {
-    // Start is called before the first frame update
-    public void KeepPlaying(string result)
+    public class EndLevelMenuScript : MonoBehaviour
     {
-        if (result == "victory" && SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        else if (result == "victory")
-            Debug.LogError("No More Scenes to load");
-        else
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    // Update is called once per frame
-    public void Levels()
-    {
-        SceneManager.LoadScene(0);
+        public void KeepPlaying(string result)
+        {
+            if (result == "victory" && SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            else if (result == "victory")
+                Debug.LogError("No More Scenes to load");
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void Levels()
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
