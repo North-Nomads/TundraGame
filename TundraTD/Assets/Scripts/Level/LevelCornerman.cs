1ac9a13daaf01d0cbf953ef8e0180c13a313cd15
Mikov Denis <denis.mikov.06@gmail.com>
2023-02-06 23:50:01 +0300

    docs(waves): added incode comments

--- TundraTD/Assets/Scripts/Level/LevelCornerman.cs
+++ TundraTD/Assets/Scripts/Level/LevelCornerman.cs
@@ -32,10 +32,14 @@ namespace Level
         
         private IEnumerator StartWavesLoop()
         {
+            // Main loop that handles beginning and ending of all waves
+            
+            // Wait for all portals instantiate themselves
             yield return new WaitUntil(() => mobPortals.All(x => x.IsInstantiated));
             
             for (int i = 0; i < _maxWavesAmongPortals; i++)
             {
+                // Start mob spawning IEnumerator for each of the portals and wait for all mobs to die 
                 foreach (var mobPortal in mobPortals)
                 {
                     mobPortal.EquipNextWave();
@@ -43,12 +47,14 @@ namespace Level
                 }
                 yield return new WaitUntil(() => mobPortals.Sum(x => x.MobsLeftThisWave) == 0);
                 
+                // Handle the ending of the wave (from all portals)
                 foreach (var mobPortal in mobPortals) 
                     mobPortal.OnWaveEnded();
                 
                 if (i != _maxWavesAmongPortals - 1)
                     yield return ShowTimerBetweenWaves();
             }
+            // After all waves were completed 
             levelJudge.HandlePlayerVictory();
         }
 
@@ -65,6 +71,7 @@ namespace Level
 
         private IEnumerator StartMobSpawning(MobPortal portal)
         {
+            // Spawn all mobs with a short delay
             for (int i = 0; i < portal.TotalWaveMobQuantity; i++)
             {
                 yield return new WaitForSeconds(mobSpawnDelay);
