a0ec39a2a1495513acf17a412e4fa47ebda80c88
Mikov Denis <denis.mikov.06@gmail.com>
2023-02-06 23:25:12 +0300

    refactor(building): fixed a typo in fire mod description

--- TundraTD/Assets/Scripts/City/Building/Upgrades/FireTowerUpgrades/WaterEnemiesDamageUpgrade.cs
+++ TundraTD/Assets/Scripts/City/Building/Upgrades/FireTowerUpgrades/WaterEnemiesDamageUpgrade.cs
@@ -9,7 +9,7 @@ namespace City.Building.Upgrades.FireTowerUpgrades
         public BasicElement Element => BasicElement.Fire;
         public int Price => 25;
         public int RequiredLevel => 1;
-        public string UpgradeDescriptionText => "Increase damege on water mobs by 5%";
+        public string UpgradeDescriptionText => "Increase damage on water mobs by 5%";
         public Sprite Sprite => Resources.Load<Sprite>("UpgradeIcons/Arcanist1");
 
         public void ExecuteOnUpgradeBought()
