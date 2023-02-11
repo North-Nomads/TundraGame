da934fb5e94f1747b3a11fceb977d5945b42e678
EveRWhateveR-python <65110318+EveRWhateveR-python@users.noreply.github.com>
2023-02-11 13:58:14 +0300

    feat(ui): smooth reduction of gate health

--- TundraTD/Assets/Scripts/City/CityGatesUI.cs
+++ TundraTD/Assets/Scripts/City/CityGatesUI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,12 +13,16 @@ namespace City
     {
         [SerializeField] private Image topHealthBar;
         [SerializeField] private Text influencePointsHolder;
+        private float _targetPercent;
 
         private void Start()
         {
+
             if (topHealthBar.sprite == null)
                 throw new ArgumentNullException("topHealthBar sprite", "Value of healthbar sprite was not assigned");
             topHealthBar.fillAmount = 1f;
+            _targetPercent = topHealthBar.fillAmount;
+            StartCoroutine(PlayHealthBarAnimation());
         }
 
         public void UpdateInfluencePointsText(string text)
@@ -27,7 +32,19 @@ namespace City
 
         public void UpdateHealthBar(float percent)
         {
-            topHealthBar.fillAmount = percent;
+            _targetPercent = percent;
+        }
+        private IEnumerator PlayHealthBarAnimation()
+        {
+            while (true)
+            {
+                if (topHealthBar.fillAmount <= _targetPercent) yield return null;
+                else
+                {
+                    topHealthBar.fillAmount -= 0.001f;
+                    yield return null;
+                }
+            }
         }
     }
 }
