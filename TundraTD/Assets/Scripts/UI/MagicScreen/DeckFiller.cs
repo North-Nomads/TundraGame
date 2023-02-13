96c913a6938b6ec3519381f226d61d70ab7e27a9
Mikov Denis <denis.mikov.06@gmail.com>
2023-02-13 17:47:20 +0300

    feat(magic): replaced icons and added borders

--- TundraTD/Assets/Scripts/UI/MagicScreen/DeckFiller.cs
+++ TundraTD/Assets/Scripts/UI/MagicScreen/DeckFiller.cs
@@ -7,26 +7,19 @@ namespace UI.MagicScreen
     /// <summary>
     /// A filler that connects the deck and the elements
     /// </summary>
-    [RequireComponent(typeof(Image))]
     public class DeckFiller : MonoBehaviour
     {
         [SerializeField] private BasicElement element;
         [SerializeField] private UpperButtonElements buttonsHolder;
-        private Image _elementIcon;
-
-        private void Start()
-        {
-            _elementIcon = GetComponent<Image>();
-        }
+        [SerializeField] private Image elementIcon;
 
         public void OnButtonClick()
         {
             var empty = System.Array.Find(buttonsHolder.ElementScripts, x => x.Element == BasicElement.None);
-            if (empty != null)
-            {
-                empty.ElementIcon.sprite = _elementIcon.sprite;
-                empty.Element = element;
-            }
+            if (empty == null) return;
+            
+            empty.ElementIcon.sprite = elementIcon.sprite;
+            empty.Element = element;
         }
     }
 }
