b6bd4e1a7bfe842d41304ff760c573eefdfb3678
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-02-18 00:04:02 +0300

    pre-feat(magic): started lightningChain script

---
+++ TundraTD/Assets/LightiningBoltSpell.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+namespace Spells.SpellClasses
+{
+    public class LightiningBoltSpell : MagicSpell
+    {
+        public override void ExecuteSpell()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (!Physics.Raycast(ray, out hit, 200, ~0, QueryTriggerInteraction.Ignore))
+                return;
+            LaunchStrike(hit.transform.position, 5);
+        }
+
+        private void LaunchStrike(Vector3 origin, int strikesLeft)
+        {
+            Vector3 ClosestEnemy = new Vector3();
+            if(strikesLeft == 0)
+                return;
+            RaycastHit hit;
+            
+            if(Physics.Linecast(origin, ClosestEnemy, out hit, ~0, QueryTriggerInteraction.Ignore))
+                return;
+
+
+            LaunchStrike(hit.transform.position, strikesLeft - 1);
+        }
+    }
+}
