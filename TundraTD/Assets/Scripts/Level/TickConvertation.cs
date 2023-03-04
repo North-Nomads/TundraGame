7f6166b3d6ae0fffcdbd708d57f7079df4a1f11d
mhl-popoff <117536607+mhl-popoff@users.noreply.github.com>
2023-03-04 09:57:45 +0300

    Update TundraTD/Assets/Scripts/Level/TickConvertation.cs
    Co-authored-by: Ivan Olyanishin <56886020+IOExcept10n@users.noreply.github.com>

--- TundraTD/Assets/Scripts/Level/TickConvertation.cs
+++ TundraTD/Assets/Scripts/Level/TickConvertation.cs
@@ -3,6 +3,6 @@
     static class TickConverter
     {
         public static int SecondsToTicks(this float seconds) => (int)(second * 10);
-        public static float TickToSecond(int tick) => tick / 10;
+        public static float TicksToSeconds(this int tick) => tick / 10;
     }
 }
