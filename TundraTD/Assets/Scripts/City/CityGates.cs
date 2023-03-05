6b25e63cf17f93a07f25e9c528fd806d1fc6b079
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-03-05 22:11:28 +0300

    refactor(magic): removed absolete debugging

--- TundraTD/Assets/Scripts/City/CityGates.cs
+++ TundraTD/Assets/Scripts/City/CityGates.cs
@@ -74,8 +74,6 @@ namespace City
            
             var mob = other.GetComponent<MobBehaviour>();
             var mobAttack = mob.MobModel.CurrentMobDamage;
-            
-            //Debug.Log($"{mob.name} attacked tower with {mobAttack} damage");
             CurrentCityGatesHealthPoints -= mobAttack;
             mob.HitThisMob(float.PositiveInfinity, BasicElement.None, "City.Gates");
             _animator.SetTrigger("DamageTrigger");
