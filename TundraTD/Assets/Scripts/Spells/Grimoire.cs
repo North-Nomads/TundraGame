56615ad01f689180276fe32e686a7b22e4460240
Mikov Denis <denis.mikov.06@gmail.com>
2023-03-05 21:03:35 +0300

    fix(magic): debug input now supports input

--- TundraTD/Assets/Scripts/Spells/Grimoire.cs
+++ TundraTD/Assets/Scripts/Spells/Grimoire.cs
@@ -30,28 +30,26 @@ namespace Spells
         /// Turns elements into a spell if it's available.
         /// </summary>
         /// <param name="elements">Elements list.</param>
+        /// <param name="hitInfo"></param>
         /// <returns>Created spell which is ready to cast.</returns>
-        public static MagicSpell TurnElementsIntoSpell(List<BasicElement> elements, RaycastHit hitInfo)
+        public static void TurnElementsIntoSpell(List<BasicElement> elements, RaycastHit hitInfo)
         {
             BasicElement? mostElement = elements.GroupBy(x => x).FirstOrDefault(x => x.Count() >= 3)?.Key;
             
-            if (!mostElement.HasValue)
-                return null;
+            if (!mostElement.HasValue) return;
 
             elements.Sort();
             int startMostIndex = elements.IndexOf(mostElement.Value); 
             var remainingElements = elements.Where((x, i) => x != mostElement || i >= startMostIndex + 3);
 
-            if (!SpellTypes.TryGetValue(mostElement.Value, out Type spellType))
-                return null;
-            
+            if (!SpellTypes.TryGetValue(mostElement.Value, out Type spellType)) return;
+
             var spellObject = SpellInitializers[(int)Math.Log((int)mostElement, 2)];
             
             // don't call any spell if this spell was not implemented (for alpha purposes only)
             // TODO: Remove
-            if (spellObject is null)
-                return null;
-            
+            if (spellObject is null) return;
+
             var spell = Object.Instantiate(spellObject);
             
             foreach (var prop in spellType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
@@ -61,7 +59,6 @@ namespace Spells
             
             spell.SpellCameraLock += HandleSpellCameraLock;
             spell.ExecuteSpell(hitInfo);
-            return spell;
         }
 
         private static void HandleSpellCameraLock(object sender, bool value)
