3a8c4bae7f17cfebec69e80ec80c77b10cd7ec5a
Даниил Шевкунов <52647986+DaSh-More@users.noreply.github.com>
2023-03-08 17:17:42 +0300

    feat(ui): add version text
    Added the text of the version in the lower left corner

---
+++ TundraTD/Assets/Scripts/SceneManagement/VersionText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Global
+{
+    public class VersionText : MonoBehaviour
+    {
+        [SerializeField]
+        private Text VText;
+
+        private void Awake()
+        {
+            VText.text = "Alpha " + Application.version;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
