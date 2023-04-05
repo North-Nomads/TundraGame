ced07023880f12452a1454394fae0b079f35f9ee
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2023-04-05 18:48:31 +0500

    feat(magic): Working on lava spell

--- TundraTD/Assets/Scripts/Mobs/MobEffects/BurnEffect.cs
+++ TundraTD/Assets/Scripts/Mobs/MobEffects/BurnEffect.cs
@@ -3,7 +3,7 @@ using Spells;
 
 namespace Mobs.MobEffects
 {
-    public class MeteoriteBurningEffect : Effect
+    public class BurnEffect : Effect
     {
         private float BurningDamage { get; }
 
@@ -13,7 +13,7 @@ namespace Mobs.MobEffects
 
         public override VisualEffectCode Code => VisualEffectCode.MeteoriteBurning;
 
-        public MeteoriteBurningEffect(float burningDamage, int maxTicksAmount, bool canBeExtinguished = true)
+        public BurnEffect(float burningDamage, int maxTicksAmount, bool canBeExtinguished = true)
         {
             BurningDamage = burningDamage;
             MaxTicksAmount = maxTicksAmount;
--- TundraTD/Assets/Scripts/Spells/SpellClasses/LavaSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/LavaSpell.cs
@@ -1,14 +1,42 @@
+using Level;
+using Mobs.MobEffects;
+using Mobs.MobsBehaviour;
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Internal;
 
 namespace Spells
 {
+    /// <summary>
+    /// The spell with lava pool which hits all enemies who are standing on it.
+    /// </summary>
 	public class LavaSpell : MagicSpell
 	{
+        [SerializeField] private float lifetime;
+        [SerializeField] private float damage;
+        [SerializeField] private float burnDamage;
+        [SerializeField] private float burnTime;
+
         public override BasicElement Element => BasicElement.Fire;
 
         public override void ExecuteSpell(RaycastHit hitInfo)
         {
-            throw new System.NotImplementedException();
+            transform.position = hitInfo.point;
+            StartCoroutine(StayAlive());
+        }
+
+        IEnumerator StayAlive()
+        {
+            yield return new WaitForSeconds(lifetime);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Mob"))
+            {
+                other.GetComponent<MobBehaviour>().AddSingleEffect(new BurnEffect(burnDamage, burnTime.SecondsToTicks()));
+            }
         }
     }
 }
