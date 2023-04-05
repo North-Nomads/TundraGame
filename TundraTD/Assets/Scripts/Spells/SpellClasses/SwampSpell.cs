ab6be69473fa1beabbc09a3ccf890f45d1c2b327
mhl_popoff <mhl_popoff@mail.ru>
2023-04-05 20:40:23 +0300

    feat(magic): add swamp

--- TundraTD/Assets/Scripts/Spells/SpellClasses/SwampSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/SwampSpell.cs
@@ -1,14 +1,42 @@
+using Level;
+using Mobs.MobEffects;
+using Mobs.MobsBehaviour;
+using System.Collections;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Internal;
 
 namespace Spells
 {
 	public class SwampSpell : MagicSpell
 	{
+        [SerializeField] private float lifetime;
+
+        [SerializeField] private float slowdownTime;
+
         public override BasicElement Element => BasicElement.Earth | BasicElement.Water;
 
         public override void ExecuteSpell(RaycastHit hitInfo)
         {
-            throw new System.NotImplementedException();
+            transform.position = hitInfo.point;
+            StartCoroutine(StayAlive());
         }
+
+        IEnumerator StayAlive()
+        {
+            yield return new WaitForSeconds(lifetime);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Mob") && Mathf.Abs(other.transform.position.y - transform.position.y) < 1.5f)
+            {
+                var mob = other.GetComponent<MobBehaviour>();
+                if (!mob.CurrentEffects.OfType<SlownessEffect>().Any())
+                    mob.AddSingleEffect(new SlownessEffect(0.5f, slowdownTime.SecondsToTicks()));
+            }
+        }
+
     }
 }
