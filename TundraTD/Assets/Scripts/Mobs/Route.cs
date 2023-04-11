48262a04744213afae9a55b20cce3ff785fd29ca
Mikov Denis <denis.mikov.06@gmail.com>
2023-04-11 22:10:00 +0300

    feat(mob): implemented basic route implementation

---
+++ TundraTD/Assets/Scripts/Mobs/Route.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Mobs
+{
+    /// <summary>
+    /// A class used to manage the waypoint system
+    /// </summary>
+    public class Route : MonoBehaviour
+    {
+        [Tooltip("An array of points, the last one must be the gates")][SerializeField] private WayPoint[] wayPoints;
+        public WayPoint[] WayPoints => wayPoints;
+    }
+}
