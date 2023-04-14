8db1d66423aaf05213f568d1b364d8dbca524410
Mikov Denis <denis.mikov.06@gmail.com>
2023-04-14 06:51:02 +0300

    feat(mob): implemented effects applying

---
+++ TundraTD/Assets/Scripts/Mobs/MobEffects/InspirationEffect.cs
@@ -0,0 +1,11 @@
+﻿namespace Mobs.MobEffects
+{
+    /// <summary>
+    /// Doubles the duration left on each effect  
+    /// </summary>
+    public class InspirationEffect : Effect
+    {
+        public override int MaxTicksAmount => 1;
+        public override VisualEffectCode Code => VisualEffectCode.Stun; // Set visual effect for inspiration later
+    }
+}
--- TundraTD/Assets/Scripts/Spells/SpellClasses/StoneGrenadeSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/StoneGrenadeSpell.cs
@@ -1,14 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mobs.MobEffects;
+using Mobs.MobsBehaviour;
 using UnityEngine;
 
-namespace Spells
+namespace Spells.SpellClasses
 {
     public class StoneGrenadeSpell : MagicSpell
     {
+        [SerializeField] private GameObject stone;
+        [SerializeField] private float liftTime;
+        [SerializeField] private float lifeTime;
+        [SerializeField] private ParticleSystem blowParticle;
+        
+        private const int MobLayer = 1 << 8; 
         public override BasicElement Element => BasicElement.Earth | BasicElement.Air;
 
-        public override void ExecuteSpell(RaycastHit hitInfo)
+        public override void ExecuteSpell(RaycastHit hitInfo) => StartCoroutine(ExecuteStoneLifting(hitInfo.point));
+        
+        private IEnumerator ExecuteStoneLifting(Vector3 position)
         {
-            throw new System.NotImplementedException();
+            var instantiatedStone = Instantiate(stone, position, Quaternion.identity, transform);
+
+            var time = 0f;
+            while (time < liftTime)
+            {
+                var lerpedY = Mathf.Lerp(0, 1, time / liftTime);
+                instantiatedStone.transform.position += Vector3.up * lerpedY;
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            var stonePosition = instantiatedStone.transform.position;
+            Instantiate(blowParticle, stonePosition, Quaternion.identity); // Destroy after executing using particle system clear behaviour
+            var mobs = Physics.OverlapSphere(stonePosition, 5f, MobLayer);
+            foreach (var overlappedMob in mobs)
+            {
+                var mob = overlappedMob.GetComponent<MobBehaviour>();
+                mob.AddReceivedEffects(new List<Effect>
+                    { new InspirationEffect(), new SpikesStunEffect(1) });
+                mob.HitThisMob(100, BasicElement.None, "Grenade");
+            }
+
+            yield return new WaitForSeconds(lifeTime);
+            Destroy(gameObject);
         }
     }
 }
