77049a889d27e182698a26e9f774fd26f510c71b
Nikitos909 <nikita034@bk.ru>
2023-04-14 19:06:59 +0300

    refactoring_wind_spell

--- TundraTD/Assets/Scripts/Spells/SpellClasses/WindSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/WindSpell.cs
@@ -19,29 +19,25 @@ namespace Spells
 
         private static readonly Collider[] AvailableTargetsPool = new Collider[1000];
 
-        [SerializeField] private MeshRenderer TornadoMesh;
         [SerializeField] private GameObject TornadoPrefab;
+        [SerializeField] private SphereCollider mainCollider;
         [SerializeField] private AudioClip TornadoSound;
-        
-        
+
         private float TornadoRadius { get; set; } = 16f;
 
-        private float TornadoSpeed { get; set; } = 1f;
+        private float SpellDuration { get; set; } = 6f;                
+        
+        public float SlownessValue { get; set; } = 0.2f;
 
-        private float SpellDuration { get; set; } = 6f;
-                
-        private float TornadoArea { get; set; } = 30f;
-
-        public float SlownessValue { get; set; } = 0.9f;
-
-        public int SlownessDuration { get; set; } = 6;
+        public float SlownessDuration { get; set; } = 6f;
 
         public override BasicElement Element => BasicElement.Air;
 
+       
         private IEnumerator WaitTime()
         {
             yield return new WaitForSeconds(SpellDuration);
-            //mainCollider.enabled = false;
+            mainCollider.enabled = false;
             yield return new WaitForSeconds(1);
             Destroy(gameObject);
         }
@@ -52,13 +48,24 @@ namespace Spells
             {
                 var mob = other.GetComponent<MobBehaviour>();
                 if (!mob.CurrentEffects.OfType<SlownessEffect>().Any())
-                    mob.AddSingleEffect(new SlownessEffect(SlownessValue, SlownessDuration));
+                    mob.AddSingleEffect(new SlownessEffect(SlownessValue, SlownessDuration.SecondsToTicks()));
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Mob"))
+            {
+                var mob = other.gameObject.GetComponent<MobBehaviour>();
+                mob.RemoveFilteredEffects(x => x is WetEffect effect);
             }
         }
 
         public override void ExecuteSpell(RaycastHit hitInfo)
         {
             transform.position = hitInfo.point;
+            mainCollider = GetComponent<SphereCollider>();
+            mainCollider.radius = TornadoRadius;
             StartCoroutine(StayAlive());
 
         }
@@ -68,6 +75,6 @@ namespace Spells
             yield return new WaitForSeconds(SpellDuration);
             Destroy(gameObject);
         }
-
+        
     }
 }
