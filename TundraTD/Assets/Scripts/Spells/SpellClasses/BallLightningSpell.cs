6357b40575d01382bf642cdfd6418b44e1abc5e1
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-04-15 14:33:46 +0300

    refactro(maginc): merged scripts

--- TundraTD/Assets/Scripts/Spells/SpellClasses/BallLightningSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/BallLightningSpell.cs
@@ -1,15 +1,45 @@
+using Mobs.MobsBehaviour;
 using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Spells
 {
     public class BallLightningSpell : MagicSpell
     {
+        private float t = 0;
+
+        [SerializeField] private float timeToDetonate;
+        [SerializeField] private float detonationRadius;
+        [SerializeField] private float maxDamage;
+        [SerializeField] private float minDamage;
         public override BasicElement Element => BasicElement.Fire | BasicElement.Lightning;
 
         public override void ExecuteSpell(RaycastHit hitInfo)
         {
             gameObject.transform.position = hitInfo.point;
+        }
+
+
+        private void Update()
+        {
+            t += Time.deltaTime;
+            if (t >= timeToDetonate)
+                Detonate();
+        }
+        private void Detonate()
+        {
+            Physics.OverlapSphere(transform.position, detonationRadius).ToList().ForEach(coll =>
+            {
+                if (coll.gameObject.TryGetComponent(out MobBehaviour mob))
+                    mob.HitThisMob(Mathf.Lerp(minDamage, maxDamage, t / timeToDetonate), BasicElement.Lightning, "Ball lightning");
+            });
+            Destroy(gameObject);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Detonate();
         }
     }
 }
--- TundraTD/Assets/Scripts/Spells/SpellClasses/LightningCrystallSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/LightningCrystallSpell.cs
@@ -1,11 +1,56 @@
 ﻿using Spells;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LightningCrystallSpell : MagicSpell
 {
+    private List<Rigidbody> _mobs = new List<Rigidbody>();
+    private float t = 0;
+
+    [SerializeField] float pullForce;
+    [SerializeField] float timeToLive;
     public override BasicElement Element => BasicElement.Earth | BasicElement.Lightning;
 
+    private void OnTriggerEnter(Collider e)
+    {
+        if (!e.gameObject.TryGetComponent(out Rigidbody enteredMob))
+            return;
+        _mobs.Add(enteredMob);
+    }
 
+    private void OnTriggerExit(Collider e)
+    {
+        if (!e.gameObject.TryGetComponent(out Rigidbody enteredMob))
+            return;
+        _mobs.Remove(enteredMob);
+    }
+
+    private void FixedUpdate()
+    {
+        _mobs.Where(mob => !mob.gameObject.activeSelf).ToList().ForEach(mob => _mobs.Remove(mob));
+
+        _mobs.ForEach(mob => mob.AddForce(new Vector3
+            (mob.transform.position.x - transform.position.x, mob.transform.position.y - transform.position.y, mob.transform.position.z - transform.position.z).normalized * pullForce * -1, ForceMode.Force));
+
+    }
+
+    private void Start()
+    {
+        Physics.OverlapSphere(transform.position, GetComponent<SphereCollider>().radius).ToList().ForEach(coll =>
+        {
+            if (coll.gameObject.TryGetComponent(out Rigidbody mob))
+                _mobs.Add(mob);
+        });
+    }
+
+    private void Update()
+    {
+        t += Time.deltaTime;
+
+        if (t > timeToLive)
+            Destroy(gameObject);
+    }
     public override void ExecuteSpell(RaycastHit hitInfo)
     {
         gameObject.transform.position = hitInfo.point;
