c5f2d4e60688673dc425db3078aa7dddd01785b5
mhl_popoff <mhl_popoff@mail.ru>
2023-04-17 19:56:14 +0300

    fix (magic) : fix-StormCloud + Del Swamp

--- TundraTD/Assets/Scripts/Spells/SpellClasses/SwampSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/SwampSpell.cs
@@ -11,14 +11,6 @@ namespace Spells
 {
 	public class SwampSpell : MagicSpell
 	{
-        [SerializeField] private float lifetime;
-        [SerializeField] private float slowdownTime;
-        private float radius_swamp = 10;
-        private float mob_position_from_center;
-        private float slowdown_mob;
-        private float slowdown_max = 0.8f;
-        private float slowdown_min = 0.2f;
-
         public override BasicElement Element => BasicElement.Earth | BasicElement.Water;
 
         public override void ExecuteSpell(RaycastHit hitInfo)
@@ -29,23 +21,10 @@ namespace Spells
 
         IEnumerator StayAlive()
         {
-            yield return new WaitForSeconds(lifetime);
+            yield return new WaitForSeconds(1);
             Destroy(gameObject);
         }
 
-        private void OnTriggerStay(Collider other)
-        {
-            if (other.CompareTag("Mob") && Mathf.Abs(other.transform.position.y - transform.position.y) < 1.5f)
-            {
-                mob_position_from_center = Vector3.Distance(transform.position, other.transform.position);
-                slowdown_mob = mob_position_from_center / radius_swamp * (slowdown_max - slowdown_min) + slowdown_min;
-
-                var mob = other.GetComponent<MobBehaviour>();
-                mob.ClearMobEffects();
-                if (!mob.CurrentEffects.OfType<SlownessEffect>().Any())
-                    mob.AddSingleEffect(new SlownessEffect(slowdown_mob, slowdownTime.SecondsToTicks()));
-            }
-        }
 
     }
 }
