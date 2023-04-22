530caa83f0ebd3abc501bf9420085718fae99882
mhl_popoff <mhl_popoff@mail.ru>
2023-04-22 08:24:03 +0300

    feat(building) : start laser

---
+++ TundraTD/Assets/Scripts/Building/LaserBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Building
+{
+    public class Laser : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer laserMesh;
+        [SerializeField] private GameObject explosionPrefab;
+        [SerializeField] private float cooldownLaser;
+        
+        private float distanceForMeteor;
+        private bool meteorInLaserRange = false;
+
+        void Start()
+        {
+            
+        }
+    }
+}
