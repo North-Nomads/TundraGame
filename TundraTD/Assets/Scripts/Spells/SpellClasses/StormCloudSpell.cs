ed7a50444ee3e2604cdd0ac6ace1e78775714949
Mikov Denis <denis.mikov.06@gmail.com>
2023-04-24 20:34:29 +0300

    aftermerge fix

--- TundraTD/Assets/Scripts/Spells/SpellClasses/StormCloudSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/StormCloudSpell.cs
@@ -1,15 +1,7 @@
-using Level;
-using Mobs.MobEffects;
-using Mobs.MobsBehaviour;
-using System.Collections;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using UnityEngine;
-using UnityEngine.Internal;
-namespace Spells
+namespace Spells.SpellClasses
 {
-    public class StormCloudSpell : MagicSpell
+    class StormCloudSpell
     {
-
+        
     }
 }
