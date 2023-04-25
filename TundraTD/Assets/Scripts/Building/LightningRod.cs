34d86252a04ee185830fdd5fe627924f4ed7bd30
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-04-25 14:55:57 +0300

    feat(buidling): added lightning rod

---
+++ TundraTD/Assets/Scripts/Building/LightningRod.cs
@@ -0,0 +1,17 @@
+﻿using Building;
+using Spells;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningRod : EnemyTower
+{
+    [SerializeField] private float effectRadius;
+    protected override void HandleSpellCast(object sender, MagicSpell.SpellCastInfo e)
+    {
+        if((e.HitInfo.point - transform.position).magnitude < effectRadius && (sender as MagicSpell).Element == BasicElement.Lightning)
+        {
+            Destroy(sender as MagicSpell);
+        }
+    }
+}
