2e353d5bcbb6bbfa312abab1bc62a77261156c34
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2023-04-27 20:47:39 +0500

    Update MagnetTower.cs

--- TundraTD/Assets/Scripts/Building/MagnetTower.cs
+++ TundraTD/Assets/Scripts/Building/MagnetTower.cs
@@ -10,11 +10,11 @@ namespace Building
         {
             var spell = (MagicSpell)sender;
             //If the spell cast point is nearby from the magnet and spell is in "blacklist", we cancel it.
-            if (((e.HitInfo.point - transform.position).sqrMagnitude < InteractionSize * InteractionSize &&
-                spell.Element == BasicElement.Fire) ||
+            if ((e.HitInfo.point - transform.position).sqrMagnitude < InteractionSize * InteractionSize &&
+                (spell.Element == BasicElement.Fire ||
                 spell.Element == (BasicElement.Water | BasicElement.Earth) ||
                 spell.Element == BasicElement.Earth ||
-                spell.Element == (BasicElement.Earth | BasicElement.Lightning))
+                spell.Element == (BasicElement.Earth | BasicElement.Lightning)))
             {
                 e.Cancel = true;
             }
