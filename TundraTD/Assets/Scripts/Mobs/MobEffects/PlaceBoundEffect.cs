77138017aaaad471ab9aa5ba0c6d2bf80c1d6885
IOExcept10n <56886020+IOExcept10n@users.noreply.github.com>
2023-05-06 21:16:45 +0500

    Update PlaceBoundEffect.cs

--- TundraTD/Assets/Scripts/Mobs/MobEffects/PlaceBoundEffect.cs
+++ TundraTD/Assets/Scripts/Mobs/MobEffects/PlaceBoundEffect.cs
@@ -1,7 +1,22 @@
+using Mobs.MobsBehaviour;
+using UnityEngine;
+
 namespace Mobs.MobEffects
 {
 	public abstract class PlaceBoundSpell : Effect
 	{
+		public Vector3 TargetPosition { get; set; }
 
-	}
+		public float Distance { get; set; }
+
+        public MobBehaviour TargetMob { get; }
+
+        public override bool ShouldBeDetached => (TargetMob.transform.position - TargetPosition).sqrMagnitude > Distance * Distance;
+
+        protected PlaceBoundSpell(Vector3 target, float distance)
+        {
+            TargetPosition = target;
+            Distance = distance;
+        }
+    }
 }
