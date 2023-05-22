ddb4958e5671b5cc3d05fc8fd7815ff5286eef61
Mikov Denis <denis.mikov.06@gmail.com>
2023-05-22 18:32:56 +0300

    feat(magic): fire tornado fix

---
+++ TundraTD/Assets/Scripts/Mobs/MobEffects/TornadoEffect.cs
@@ -0,0 +1,14 @@
+﻿namespace Mobs.MobEffects
+{
+    public class TornadoEffect : Effect
+    {
+        public override int MaxTicksAmount { get; protected set; }
+
+        public override VisualEffectCode Code => VisualEffectCode.Wet;
+
+        public TornadoEffect(int time)
+        {
+            MaxTicksAmount = time;
+        }
+    }
+}
--- TundraTD/Assets/Scripts/Spells/SpellClasses/FireStormSpell.cs
+++ TundraTD/Assets/Scripts/Spells/SpellClasses/FireStormSpell.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Level;
 using Mobs.MobEffects;
 using Mobs.MobsBehaviour;
@@ -12,60 +14,85 @@ namespace Spells.SpellClasses
         [SerializeField] private float lifetime;
         private Vector3 _tornadoCenter;
 
+        private Collider[] _mobs;
+        private BoxCollider _boxCollider;
         private List<MobBehaviour> _affectedMobs;
         public override BasicElement Element => BasicElement.Fire | BasicElement.Air;
 
         public override void ExecuteSpell(RaycastHit hitInfo)
         {
+            _boxCollider = GetComponent<BoxCollider>();
+            _boxCollider.enabled = false;
+            _mobs = new Collider[64];
             _affectedMobs = new List<MobBehaviour>();
             transform.position = hitInfo.point;
             _tornadoCenter = hitInfo.point;
             StartCoroutine(StayAlive());
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void HandleStormDestroying()
         {
-            if (other.CompareTag("Mob"))
+            foreach (var mob in _affectedMobs)
             {
-                var mob = other.GetComponent<MobBehaviour>();
+                if (!mob.MobModel.IsAlive)
+                    return;
                 
+                Debug.Log("Landing");
+                mob.ClearMobEffects();
+                mob.IsMoving = false;
+                mob.MobModel.Rigidbody.AddForce(Vector3.down * 10000);
+                mob.IsMoving = true;
+                mob.SetFocusingTarget(null);
+            }
+        }
+
+        private void ScanForMobs(int c)
+        {
+            for (int i = 0; i < c; i++)
+            {
+                var mobCollider = _mobs[i];
+                var mob = mobCollider.GetComponent<MobBehaviour>();
+                if (mob is null)
+                    continue;
+                
+                if (mob.CurrentEffects.Any(x => x is TornadoEffect))
+                    continue;
+
                 var destination = _tornadoCenter + Vector3.up * 4;
                 mob.SetFocusingTarget(destination);
                 mob.AddReceivedEffects(new List<Effect>
                 {
                     new BurningEffect(.5f, 6f.SecondsToTicks(), false),
                     new InspirationEffect(),
-                    new SpeedEffect(6f.SecondsToTicks(), 4)
+                    new SpeedEffect(6f.SecondsToTicks(), 4),
+                    new TornadoEffect(lifetime.SecondsToTicks())
                 });
                 _affectedMobs.Add(mob);
             }
         }
 
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.CompareTag("Mob"))
-            {
-                var mob = other.GetComponent<MobBehaviour>();
-                mob.SetFocusingTarget(null);
-                _affectedMobs.Remove(mob);
-            }
-        }
-
         private IEnumerator StayAlive()
         {
-            yield return new WaitForSeconds(lifetime);
-
-            // Handle landing
-            foreach (var mob in _affectedMobs)
+            float step = lifetime / 10;
+            float i = 0;
+            while (i < lifetime)
             {
-                mob.IsMoving = false;
-                mob.MobModel.Rigidbody.AddForce(Vector3.down * 1000);
-                yield return new WaitForSeconds(.3f);
-                mob.IsMoving = true;
-                mob.SetFocusingTarget(null);
+                var c = Physics.OverlapBoxNonAlloc(transform.position + Vector3.up * 2, _boxCollider.size / 2, _mobs);
+                ScanForMobs(c);
+                i += step;
+                yield return new WaitForSeconds(.5f);
             }
             
+            // Handle landing
+            print("Destroying");
+            print(_affectedMobs.Count);
+            HandleStormDestroying();
             Destroy(gameObject);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawCube(transform.position + Vector3.up * 2, _boxCollider.size / 2);
         }
     }
 }
