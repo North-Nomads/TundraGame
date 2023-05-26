5d0e1720d70bb7056943cea6842f69e02f2f2720
GalteXx <65533265+GalteXx@users.noreply.github.com>
2023-05-26 16:31:14 +0300

    fix(analytics): removed analytivcs debugging

--- TundraTD/Assets/Scripts/SceneManagement/SpellAnalytics.cs
+++ TundraTD/Assets/Scripts/SceneManagement/SpellAnalytics.cs
@@ -8,6 +8,6 @@ public class SpellAnalytics : EnemyTower
 {
     protected override void HandleSpellCast(object sender, MagicSpell.SpellCastInfo e)
     {
-        Debug.Log(Analytics.CustomEvent(sender.GetType().ToString().Split('.').Last()));
+        Analytics.CustomEvent(sender.GetType().ToString().Split('.').Last());
     }
 }
